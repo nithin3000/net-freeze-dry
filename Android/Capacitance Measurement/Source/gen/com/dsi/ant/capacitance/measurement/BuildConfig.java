eee29ae2e7c8ba83bd50353cf69af7d39658e083
jiang175 <jiang175@purdue.edu>
2014-12-02 22:53:13 -0500

    Capcitance Measure

---
+++ Android/Capacitance Measurement/Source/gen/com/dsi/ant/capacitance/measurement/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dsi.ant.capacitance.measurement;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
